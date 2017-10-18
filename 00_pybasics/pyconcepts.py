b8ae6c856a301c54a6ef2f45215d9d3e0fae8cdb
jennyd <jennyd@ita-gram-w-59.d.ethz.ch>
2017-10-18 15:12:29 +0200

    update

--- 00_pybasics/pyconcepts.py
+++ 00_pybasics/pyconcepts.py
@@ -2,3 +2,20 @@
 pyground
 author: David Jenny
 """
+
+# simple matplotlib example, random walk plot
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+
+# example on keyword module
+import keyword
+
+print (keyword.iskeyword("hello"))
+print (keyword.iskeyword("False"))
+print (keyword.kwlist)
+
+# example on json module
+import json
