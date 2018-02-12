3a7615231f9cac2a48e6d6710a6402b8d57f4d53
DavidJenny <david.jenny@arch.ethz.ch>
2018-02-12 14:50:11 +0100

    dict example added

---
+++ 00_pybasics/dict.py
@@ -0,0 +1,8 @@
+# dictionary update() function
+# similar to list extend but for dictionaries
+
+dict = {'Name': 'Zara', 'Age': 7}
+dict2 = {'Sex': 'female' }
+
+dict.update(dict2)
+print ("Value : %s" %  dict)
